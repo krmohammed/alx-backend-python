fd73bb572fe53e24117517848422d9436390027a
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-02 00:56:52 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""provides the function floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the largest integer less than or equal to the given number"""
+    return math.floor(n)
