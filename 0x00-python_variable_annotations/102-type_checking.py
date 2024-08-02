c46b6cf51a64faf81e6368631f8cde769f469d80
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-02 02:29:16 +0000

    task 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""provides the function zoom_array"""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List[Any]:
+    zoomed_in: List[Any] = [item for item in lst for i in range(int(factor))]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
