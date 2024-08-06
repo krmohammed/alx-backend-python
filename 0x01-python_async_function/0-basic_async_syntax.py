16f9c26cf629081c45f432575be7b43c2f3a7d53
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-06 15:17:17 +0000

    hot fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> asyncio.Future:
+async def wait_random(max_delay: int = 10) -> float:
     """
     takes in max_delay, with a default value of 10
     then waits for a random delay between 0 and max_delay
