9711c43e2e49ae3044e42ed613a8154038de68ac
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-06 15:16:02 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" provides the wait_random functon """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> asyncio.Future:
+    """
+    takes in max_delay, with a default value of 10
+    then waits for a random delay between 0 and max_delay
+
+    Args:
+        max_delay: maximum sleep time
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
