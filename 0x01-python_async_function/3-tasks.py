058e9d9e58cff5a9f553c5e4a1b8764a24158c99
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-07 03:30:46 +0000

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" provides the function task_wait_random """
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Future:
+    """
+    task_wait_random function
+    """
+    return asyncio.ensure_future(wait_random(max_delay))
