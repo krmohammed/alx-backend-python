3cc130c371a0cd7c934b37fca234b7f1cad46338
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-07 22:49:30 +0000

    task type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """provides the function async_generator"""
 import asyncio
 import random
-from typing import List
+from typing import AsyncGenerator
 
 
-async def async_generator() -> List[int]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """coroutine: async_generator"""
     for i in range(10):
         await asyncio.sleep(1)
