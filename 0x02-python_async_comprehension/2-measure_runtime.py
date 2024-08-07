75920ff1a8d4ad55e83eacee019365b4f9caa46b
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-07 22:39:58 +0000

    task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""provides the function measure_runtime"""
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    """measure_runtime"""
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    runtime = time.perf_counter() - start_time
+    return runtime
