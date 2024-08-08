c73076e6d416bee77f759b2c88a641d005abb7b5
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-08 00:28:46 +0000

    task 1 type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """provides the function async comprehension"""
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """async_comprehension"""
     result = [i async for i in async_generator()]
     return result
