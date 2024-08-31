9957c3804767709129f962afe79e637062be70d4
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-31 16:37:31 +0000

    task 1 fixed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
-    def test_access_nested_map_with_exeception(self, nested_map, path):
+    def test_access_nested_map_exeception(self, nested_map, path):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
             self.assertEqual(str(e.exception), f"Key '{path[-1]}' not found in the dictionary")
