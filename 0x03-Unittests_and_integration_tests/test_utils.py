90c3a70909a04ad6e3669a33a2b7874ca2566aa7
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-31 16:35:34 +0000

    task 1 fixed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,5 +19,12 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
     def test_access_nested_map_with_exeception(self, nested_map, path):
-        with self.assertRaises(KeyError):
+        """
+        Implement TestAccessNestedMap.test_access_nested_map_exception. Use the assertRaises context manager to test that a KeyError is raised for the following inputs (use @parameterized.expand):
+        nested_map={}, path=("a",)
+        nested_map={"a": 1}, path=("a", "b")
+        Also make sure that the exception message is as expected.
+        """
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+            self.assertEqual(str(e.exception), f"Key '{path[-1]}' not found in the dictionary")
