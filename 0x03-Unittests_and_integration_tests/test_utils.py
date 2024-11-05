82f4fd3de089e3c65187413709e849d6b939eeae
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-11-05 11:45:04 +0000

    hot fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 """
 import unittest
 from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,6 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ["a", "b"], 2),
         ]
     )
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+        self, nested_map: Mapping, path: Sequence, expected: Any
+    ):
         """tests for for right answers"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
